f7fbe60c78b3d78c0fb74e9b39768c83b6d93ca5
Hammers <gianluca.hammers@gmail.com>
2026-02-23 16:21:18 +0100

    FatturaService

--- src/main/java/epicode/epicenergy/entities/Cliente.java
+++ src/main/java/epicode/epicenergy/entities/Cliente.java
@@ -1,4 +1,14 @@
 package epicode.epicenergy.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import java.util.UUID;
+
+@Entity
 public class Cliente {
+    @Id
+    @GeneratedValue
+    private UUID cleinteId;
 }
