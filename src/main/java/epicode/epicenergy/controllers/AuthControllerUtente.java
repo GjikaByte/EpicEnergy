382acea102402c0a9dc0d26290d87c65d924f252
Andi Gjika <andi.gjika@outlook.com>
2026-02-24 12:19:39 +0100

    Set up and tested security with bcrypt

---
+++ src/main/java/epicode/epicenergy/controllers/AuthControllerUtente.java
@@ -0,0 +1,48 @@
+package epicode.epicenergy.controllers;
+
+import epicode.epicenergy.DTOs.*;
+import epicode.epicenergy.entities.Utente;
+import epicode.epicenergy.exceptions.ValidationException;
+import epicode.epicenergy.services.*;
+import epicode.epicenergy.services.AuthServiceUtente;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthControllerUtente {
+    private final AuthServiceUtente authService;
+    private final UtentiService utenteService;
+
+
+    public AuthControllerUtente(AuthServiceUtente authService, UtentiService utenteService) {
+        this.authService = authService;
+        this.utenteService = utenteService;
+    }
+
+    @PostMapping("/login")
+    public LoginResponseDTO login(@RequestBody LoginDTO body) {
+
+        return new LoginResponseDTO(this.authService.checkCredentialsAndGenerateToken(body));
+    }
+
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Utente createUtente(@RequestBody @Validated UtenteDTO payload, BindingResult validationResult) {
+
+        if (validationResult.hasErrors()) {
+            List<String> errorsList = validationResult.getFieldErrors()
+                    .stream()
+                    .map(fieldError -> fieldError.getDefaultMessage())
+                    .toList();
+
+            throw new ValidationException(errorsList);
+        } else {
+            return this.utenteService.save(payload);
+        }
+    }
+}
