1ebf10d4909635bfd2d455e1b2e6097da50070d6
KeviN-XVII <kevinquartar@gmail.com>
2026-02-27 12:01:49 +0100

    SetAvatar

--- src/main/java/epicode/epicenergy/services/UtentiService.java
+++ src/main/java/epicode/epicenergy/services/UtentiService.java
@@ -58,6 +58,7 @@ public class UtentiService {
                 bcrypt.encode(payload.password()),
                 found
         );
+        newUtente.setAvatar("https://ui-avatars.com/api/?name="+payload.nome()+"+"+payload.cognome());
 
 
         Utente savedUtente = this.utentiRepository.save(newUtente);
